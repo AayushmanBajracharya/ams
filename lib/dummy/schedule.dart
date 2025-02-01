36ac11ea23d7d090d1f4d6e95947d11895b03f92
AayushmanBajracharya <aayushmanbajracharya5@gmail.com>
2025-02-01 18:55:21 +0545

    teacher dashboard

---
+++ lib/dummy/schedule.dart
@@ -0,0 +1,56 @@
+List<Map<String, dynamic>> scheduleData = [
+  {
+    "className": "ENE101",
+    "startTime": "11:00",
+    "endTime": "13:00",
+    "dayOfWeek": "tuesday"
+  },
+  {
+    "className": "ENE101",
+    "startTime": "11:00",
+    "endTime": "13:00",
+    "dayOfWeek": "tuesday"
+  },
+  {
+    "className": "ENE101",
+    "startTime": "11:00",
+    "endTime": "13:00",
+    "dayOfWeek": "tuesday"
+  },
+  {
+    "className": "ENE101",
+    "startTime": "11:00",
+    "endTime": "13:00",
+    "dayOfWeek": "tuesday"
+  },
+  {
+    "className": "ENE101",
+    "startTime": "11:00",
+    "endTime": "13:00",
+    "dayOfWeek": "tuesday"
+  },
+  {
+    "className": "ENE101",
+    "startTime": "11:00",
+    "endTime": "13:00",
+    "dayOfWeek": "tuesday"
+  },
+  {
+    "className": "ENE101",
+    "startTime": "11:00",
+    "endTime": "13:00",
+    "dayOfWeek": "tuesday"
+  },
+  {
+    "className": "ENE101",
+    "startTime": "11:00",
+    "endTime": "13:00",
+    "dayOfWeek": "tuesday"
+  },
+  {
+    "className": "ENE101",
+    "startTime": "11:00",
+    "endTime": "13:00",
+    "dayOfWeek": "tuesday"
+  },
+];
--- lib/teacher_my_classes_screen.dart
+++ lib/teacher_my_classes_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -272,6 +273,7 @@ class _TeacherMyClassState extends State<TeacherMyClass> {
     String selectedDay = '';
     TimeOfDay? startTime;
     TimeOfDay? endTime;
+    String classId;
 
     // List of days for the dropdown
     final List<String> daysOfWeek = [
